46ac5137d1cde6325b2b3026b8e37ce660ff6c17
Saurav <saurav.chaudhary3355@gmail.com>
2025-04-10 16:33:02 -0400

    updated url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://cliq-rhp7.com";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://cliq-rhp7.onrender.com";
 
 export default API_BASE_URL;
