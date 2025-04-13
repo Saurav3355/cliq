882e4baa28cb401b44bac23fbc23c9719aaf4110
Saurav <saurav.chaudhary3355@gmail.com>
2025-04-13 16:28:15 -0400

    updated project

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoCloudUri = "mongodb+srv://sauravchaudhary3355:qmmdOIn38McXTWaF@cliques-db.8uor3.mongodb.net/cliques"
-const connectToMongo = () => {
-    mongoose.connect(mongoCloudUri, () => {
-        console.log("Connected to mongoDB successfully.")
-    })
-}
-module.exports = connectToMongo
--- frontend/src/config.js
+++
@@ -1,6 +0,0 @@
-// src/config.js
-
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://cliq-rhp7.onrender.com";
-
-export default API_BASE_URL;
-//test comment
